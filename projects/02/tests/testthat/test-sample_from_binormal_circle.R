18bea7772d2bce381c8e0d8d7b4277a62d539452
wlangera <ward.langeraert@inbo.be>
2024-03-13 12:09:23 +0100

    remove whitespace

--- projects/02/tests/testthat/test-sample_from_binormal_circle.R
+++ projects/02/tests/testthat/test-sample_from_binormal_circle.R
@@ -110,7 +110,6 @@ test_that("output class is correct", {
                   class = "data.frame")
 })
 
-
 test_that("coordinateUncertaintyInMeters column is handled correctly", {
   # in case of missing initial coordinateUncertaintyInMeters column
   ## no seed
