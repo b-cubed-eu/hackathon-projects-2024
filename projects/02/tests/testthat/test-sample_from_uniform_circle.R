3c1c0fc6c2481e6c89d6e3104ad9998a1b879429
wlangera <ward.langeraert@inbo.be>
2024-03-13 10:53:07 +0100

    create example datasets

--- projects/02/tests/testthat/test-sample_from_uniform_circle.R
+++ projects/02/tests/testthat/test-sample_from_uniform_circle.R
@@ -1,6 +1,29 @@
 # Source functions
 source(here("R", "sample_from_uniform_circle.R"))
 
+# Load packages
+library(sf)
+library(dplyr)
+
+# Prepare example datasets
+## number of points and extend
+n_points <- 4
+xlim <- c(3841000, 3842000)
+ylim <- c(3110000, 3112000)
+
+## dataset without coordinateUncertaintyInMeters
+observations_sf1 <- data.frame(
+  lat = runif(n_points, ylim[1], ylim[2]),
+  long = runif(n_points, xlim[1], xlim[2])
+  ) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 3035)
+
+## dataset with coordinateUncertaintyInMeters
+set.seed(123)
+coordinate_uncertainty <- rgamma(n_points, shape = 5, rate = 0.1)
+observations_sf2 <- observations_sf1 %>%
+  mutate(coordinateUncertaintyInMeters = coordinate_uncertainty)
+
 # Unit tests
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
